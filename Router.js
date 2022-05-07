97464a32d8f804cc577f673e61e5afd4566503ce
Pulkit Gupta <76155456+pulkit-30@users.noreply.github.com>
2022-05-07 11:17:10 +0530

    some major changes and deployed

--- Router.js
+++ Router.js
@@ -1,22 +1,7 @@
-const search = require('./router/search');
-const auth = require('./router/auth');
-const message = require('./router/messages');
 /**
  * define App Routes
  * @param {Object} app
  */
-const routes = (app) => {
-  /**
-   * User Authentication
-   * path : /api/auth
-   */
-  app.use('/api/auth', auth);
-  /**
-   * Search Query
-   * path : /api/search
-   */
-  app.use('/api/search', search);
-  app.use('/api/message', message);
-};
+const routes = (app) => {};
 
 module.exports = routes;
--- app.js
+++ app.js
@@ -6,15 +6,32 @@ app.use(cors());
 // DB_connection
 const db_connect = require('./connection/db_connect');
 db_connect();
+const search = require('./router/search');
+const auth = require('./router/auth');
+const message = require('./router/messages');
 
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
 // App Routes
-const Routes = require('./Router');
-Routes(app);
+/**
+ * User Authentication
+ * path : /api/auth
+ */
+app.use('/api/auth', auth);
+/**
+ * Search Query
+ * path : /api/search
+ */
+app.use('/api/search', search);
+/**
+ * Messages
+ * path : /api/message
+ */
+app.use('/api/message', message);
 
+// App Listening
 const PORT = process.env.PORT || '8080';
 app.listen(PORT, () => {
   console.log('server is running at port : ', PORT);
